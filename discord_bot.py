3920b8a853cc585a7d1b17b7d6f4a3617f41d91c
wkdgmr <wkdoov@gmail.com>
2023-04-14 11:14:25 -0400

    adding wkdmod to game list text

--- discord_bot.py
+++ discord_bot.py
@@ -37,9 +37,9 @@ def formatGame(game):
     elif game['type'] ==  'DRDX':
         text += ' <:diabloico:760201452957335552> X'
     elif game['type'] ==  'DWKD':
-        text += ' <:diabloico:760201452957335552>'
+        text += ' <:diabloico:760201452957335552> wkdmod'
     elif game['type'] ==  'HWKD':
-        text += ' <:hellfire:766901810580815932>'
+        text += ' <:hellfire:766901810580815932> wkdmod'
     else:
         text += ' ' + game['type']
 
