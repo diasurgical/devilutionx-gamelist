c64a985f194e11f09bdbf5b92596e537ef8982b2
KPhoenix <ericjrobinson@live.com>
2023-10-01 23:07:28 -0400

    Clean up Python code

--- discord_bot.py
+++ discord_bot.py
@@ -1,5 +1,4 @@
 import discord
-import subprocess
 import json
 import time
 import asyncio
@@ -7,65 +6,73 @@ import datetime
 import math
 import re
 
+# Constants
+DISCORD_CHANNEL_ID = 1061483226767556719
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-currentOnline = 0
-globalOnlineListMessage = -1
-globalChannel = -1
-gameTTL = 120 # games are marked as active for x seconds every time they show up
+current_online = 0
+global_online_list_message = None
+global_channel = -1
+gameTTL = 120  # games are marked as active for x seconds every time they show up
 
-def escapeDiscordFormattingCharacters(text: str):
+
+def escape_discord_formatting_characters(text: str):
     return re.sub(r'([-\\*_#|~:@[\]()<>`])', r'\\\1', text)
 
-def formatGame(game):
+
+def format_game(game):
     global gameTTL
     ended = time.time() - game['last_seen'] >= gameTTL
     if ended:
         text = '~~' + game['id'].upper() + '~~'
     else:
         text = '**' + game['id'].upper() + '**'
-    if game['type'] == 'DRTL':
-        text += ' <:diabloico:760201452957335552>'
-    elif game['type'] == 'DSHR':
-        text += ' <:diabloico:760201452957335552> (spawn)'
-    elif game['type'] == 'HRTL':
-        text += ' <:hellfire:766901810580815932>'
-    elif game['type'] == 'HSHR':
-        text += ' <:hellfire:766901810580815932> (spawn)'
-    elif game['type'] == 'IRON':
-        text += ' Ironman'
-    elif game['type'] == 'MEMD':
-        text += ' <:one_ring:1061898681504251954>'
-    elif game['type'] ==  'DRDX':
-        text += ' <:diabloico:760201452957335552> X'
-    elif game['type'] ==  'DWKD':
-        text += ' <:mod_wkd:1097321063077122068> modDiablo'
-    elif game['type'] ==  'HWKD':
-        text += ' <:mod_wkd:1097321063077122068> modHellfire'
-    else:
-        text += ' ' + game['type']
+        match game['type']:
+            case 'DRTL':
+                text += ' <:diabloico:760201452957335552>'
+            case 'DSHR':
+                text += ' <:diabloico:760201452957335552> (spawn)'
+            case 'HRTL':
+                text += ' <:hellfire:766901810580815932>'
+            case 'HSHR':
+                text += ' <:hellfire:766901810580815932> (spawn)'
+            case 'IRON':
+                text += ' Ironman'
+            case 'MEMD':
+                text += ' <:one_ring:1061898681504251954>'
+            case 'DRDX':
+                text += ' <:diabloico:760201452957335552> X'
+            case 'DWKD':
+                text += ' <:mod_wkd:1097321063077122068> modDiablo'
+            case 'HWKD':
+                text += ' <:mod_wkd:1097321063077122068> modHellfire'
+            case _:
+                text += ' ' + game['type']
 
     text += ' ' + game['version']
 
-    if game['tick_rate'] == 20:
-        text += ''
-    elif game['tick_rate'] == 30:
-        text += ' Fast'
-    elif game['tick_rate'] == 40:
-        text += ' Faster'
-    elif game['tick_rate'] == 50:
-        text += ' Fastest'
-    else:
-        text += ' speed: ' + str(game['tick_rate'])
+    match game['tick_rate']:
+        case 20:
+            text += ''
+        case 30:
+            text += ' Fast'
+        case 40:
+            text += ' Faster'
+        case 50:
+            text += ' Fastest'
+        case _:
+            text += ' speed: ' + str(game['tick_rate'])
 
-    if game['difficulty'] == 0:
-        text += ' Normal'
-    elif game['difficulty'] == 1:
-        text += ' Nightmare'
-    elif game['difficulty'] == 2:
-        text += ' Hell'
+    match game['difficulty']:
+        case 0:
+            text += ' Normal'
+        case 1:
+            text += ' Nightmare'
+        case 2:
+            text += ' Hell'
 
     attributes = []
     if game['run_in_town']:
@@ -84,41 +91,44 @@ def formatGame(game):
         text += ', '.join(attributes)
         text += ')'
 
-    text += '\nPlayers: **' + '**, **'.join([escapeDiscordFormattingCharacters(name) for name in game['players']]) + '**'
+    text += '\nPlayers: **' + '**, **'.join([escape_discord_formatting_characters(name) for name in game['players']]) + '**'
     text += '\nStarted: <t:' + str(round(game['first_seen'])) + ':R>'
     if ended:
-        text += '\nEnded after: `' + formatTimeDelta(round((time.time() - game['first_seen']) / 60)) + '`'
+        text += '\nEnded after: `' + format_time_delta(round((time.time() - game['first_seen']) / 60)) + '`'
 
     return text
 
-async def updateStatusMessage():
-    global currentOnline
-    global globalChannel
-    global globalOnlineListMessage
-    if (globalOnlineListMessage != -1):
+
+async def update_status_message():
+    global current_online
+    global global_channel
+    global global_online_list_message
+    if global_online_list_message != -1:
         try:
-            await globalOnlineListMessage.delete()
+            await global_online_list_message.delete()
         except discord.errors.NotFound:
             pass
-        globalOnlineListMessage = -1
-    text = 'There are currently **' + str(currentOnline) + '** public games.'
-    if currentOnline == 1:
-        text = 'There is currently **' + str(currentOnline) + '** public game.'
-    globalOnlineListMessage = await globalChannel.send(text)
+        global_online_list_message = None
+    text = 'There are currently **' + str(current_online) + '** public games.'
+    if current_online == 1:
+        text = 'There is currently **' + str(current_online) + '** public game.'
+    global_online_list_message = await global_channel.send(text)
 
-async def updateGameMessage(gameId):
-    global globalChannel
-    text = formatGame(gameList[gameId])
-    if 'message' in gameList[gameId]:
-        if (gameList[gameId]['message'].content != text):
+
+async def update_game_message(game_id):
+    global global_channel
+    text = format_game(game_list[game_id])
+    if 'message' in game_list[game_id]:
+        if game_list[game_id]['message'].content != text:
             try:
-                await gameList[gameId]['message'].edit(content=text)
+                await game_list[game_id]['message'].edit(content=text)
             except discord.errors.NotFound:
                 pass
         return
-    gameList[gameId]['message'] = await globalChannel.send(text)
+    game_list[game_id]['message'] = await global_channel.send(text)
 
-def formatTimeDelta(minutes):
+
+def format_time_delta(minutes):
     if minutes < 2:
         return '1 minute'
     elif minutes < 60:
@@ -133,28 +143,31 @@ def formatTimeDelta(minutes):
         text += str(hours) + ' hours'
         minutes -= hours * 60
 
-    if (minutes > 0):
-        text += ' and ' + formatTimeDelta(minutes)
+    if minutes > 0:
+        text += ' and ' + format_time_delta(minutes)
 
     return text
 
-async def endGameMessage(gameId):
-    if 'message' in gameList[gameId]:
+
+async def end_game_message(game_id):
+    if 'message' in game_list[game_id]:
         try:
-            await gameList[gameId]['message'].edit(content=formatGame(gameList[gameId]))
+            await game_list[game_id]['message'].edit(content=format_game(game_list[game_id]))
         except discord.errors.NotFound:
             pass
 
-async def removeGameMessages(gameIds):
-    for gameId in gameIds:
-        if 'message' in gameList[gameId]:
+
+async def remove_game_messages(game_ids):
+    for gameId in game_ids:
+        if 'message' in game_list[gameId]:
             try:
-                await gameList[gameId]['message'].delete()
+                await game_list[gameId]['message'].delete()
             except discord.errors.NotFound:
                 pass
-            del gameList[gameId]['message']
+            del game_list[gameId]['message']
 
-def anyPlayerNameIsInvalid(players):
+
+def any_player_name_is_invalid(players):
     for name in players:
         # using the same restricted character list as DevilutionX, see
         #  https://github.com/diasurgical/devilutionX/blob/0eda8d9367e08cea08b2ad81e1ce534e927646d6/Source/DiabloUI/diabloui.cpp#L649
@@ -170,9 +183,10 @@ def anyPlayerNameIsInvalid(players):
 
     return False
 
-def anyPlayerNameContainsABannedWord(players):
-    with open('./banlist', 'r') as file:
-        words = set([line.strip().upper() for line in file.read().split('\n') if line.strip()])
+
+def any_player_name_contains_a_banned_word(players):
+    with open('./banlist', 'r') as ban_list_file:
+        words = set([line.strip().upper() for line in ban_list_file.read().split('\n') if line.strip()])
 
         for name in players:
             for word in words:
@@ -181,17 +195,20 @@ def anyPlayerNameContainsABannedWord(players):
 
     return False
 
-gameList = {}
-backgroundTaskRunning = 0
-async def backgroundTask():
+
+game_list = {}
+background_task_running = 0
+
+
+async def background_task():
     global gameTTL
-    global currentOnline
-    lastRefresh = 0
-    refreshSeconds = 60 #refresh gamelist every x seconds
+    global current_online
+    last_refresh = 0
+    refresh_seconds = 60  # refresh gamelist every x seconds
     while True:
         await asyncio.sleep(1)
-        if time.time() - lastRefresh >= refreshSeconds:
-            lastRefresh = time.time()
+        if time.time() - last_refresh >= refresh_seconds:
+            last_refresh = time.time()
             # Call the external program and get the output
             proc = await asyncio.create_subprocess_shell(
                 './devilutionx-gamelist',
@@ -210,52 +227,52 @@ async def backgroundTask():
             print('[' + str(ct) + '] Refreshing game list - ' + str(len(games)) + ' games')
 
             for game in games:
-                if anyPlayerNameIsInvalid(game['players']) or anyPlayerNameContainsABannedWord(game['players']):
+                if any_player_name_is_invalid(game['players']) or any_player_name_contains_a_banned_word(game['players']):
                     continue
 
                 key = game['id'].upper()
-                if key in gameList:
-                    gameList[key]['players'] = game['players']
-                    gameList[key]['last_seen'] = time.time()
+                if key in game_list:
+                    game_list[key]['players'] = game['players']
+                    game_list[key]['last_seen'] = time.time()
                     continue
 
-                gameList[key] = game
-                gameList[key]['first_seen'] = time.time()
-                gameList[key]['last_seen'] = time.time()
+                game_list[key] = game
+                game_list[key]['first_seen'] = time.time()
+                game_list[key]['last_seen'] = time.time()
 
-            endedGames = []
-            for key, game in gameList.items():
+            ended_games = []
+            for key, game in game_list.items():
                 if time.time() - game['last_seen'] < gameTTL:
                     continue
-                endedGames.append(key)
-                await endGameMessage(key)
+                ended_games.append(key)
+                await end_game_message(key)
 
-            for key in endedGames:
-                del gameList[key]
+            for key in ended_games:
+                del game_list[key]
 
-            if len(endedGames) != 0:
-                await removeGameMessages(gameList.keys())
+            if len(ended_games) != 0:
+                await remove_game_messages(game_list.keys())
 
-            for gameId in gameList.keys():
-                await updateGameMessage(gameId)
+            for gameId in game_list.keys():
+                await update_game_message(gameId)
 
-            if (currentOnline == len(gameList)) or len(endedGames) != 0:
+            if (current_online == len(game_list)) or len(ended_games) != 0:
                 continue
 
-            currentOnline = len(gameList)
-            await updateStatusMessage()
+            current_online = len(game_list)
+            await update_status_message()
 
-            activity = discord.Activity(name='Games online: '+str(currentOnline), type=discord.ActivityType.watching)
+            activity = discord.Activity(name='Games online: '+str(current_online), type=discord.ActivityType.watching)
             await client.change_presence(activity=activity)
+
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    global globalChannel
-    globalChannel = client.get_channel(1061483226767556719)
-    await backgroundTask()
+    global global_channel
+    global_channel = client.get_channel(DISCORD_CHANNEL_ID)
+    await background_task()
 
-token = ''
 with open('./discord_bot_token', 'r') as file:
     token = file.readline()
 
